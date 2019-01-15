3e8d8935ecefc4fbea8c84b7684d6c411c436564
Dizan P <dizanp@gmail.com>
2019-01-15 16:24:43 +0700

    first commit

---
+++ script1.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+  function slide(num){
+    if($('.container').css('marginLeft') == '-2000px')
+      $('.container').css('marginLeft', '0px');
+    else
+      $('.container').animate({marginLeft: num});
+  }
+
+  var autoslide = setInterval(function(){
+    slide('-=400px');
+  }, 2000);
+
+  $('#next').click(function(){
+    if($('.container').css('marginLeft') == '-1600px')
+      $('.container').css('marginLeft', '0px');
+    else
+      slide('-=400px');
+      clearInterval(autoslide);
+  });
+
+  $('#prev').click(function(){
+    if($('.container').css('marginLeft') == '0px')
+      $('.container').css('marginLeft', '-1600px');
+    else
+      slide('+=400px');
+      clearInterval(autoslide);
+  });
+
+});
